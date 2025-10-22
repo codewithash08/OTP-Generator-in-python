2dd1fd4caedd5ad0d00d386e9618971b9526ab65
Ash <ashwani555q@gmail.com>
2025-10-22 22:52:35 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+
+import math
+
+
+import random
+otp_2 =  (random.randint(1, 9))
+otp_3 =  (random.randint(1, 9))
+otp_4 =  (random.randint(1, 9))
+otp_5=   (random.randint(1, 9))
+otp_6=   (random.randint(1, 9))
+otp_7=   (random.randint(1, 9))
+otp_8=   (random.randint(1, 9))
+
+print ("\n")
+print ("Otp Generator ----\n")
+
+print ( otp_2 , otp_3 , otp_4 , otp_5 , otp_6 , otp_7 , otp_8 )
+
+print ("\n")
+
+
+
+    
